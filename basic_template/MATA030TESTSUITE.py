0bff8393b79356aa562e9e6512c11ee6e039b177
Renan Lacerda Lisboa <renanllisboa@gmail.com>
2021-02-03 10:53:21 -0300

    modify testcase in suite

--- basic_template/MATA030TESTSUITE.py
+++ basic_template/MATA030TESTSUITE.py
@@ -4,7 +4,7 @@ from MATA030TESTCASE import MATA030
 
 suite = unittest.TestSuite()
 
-suite.addTest(MATA030('test_MATA030_CT001'))
+suite.addTest(MATA030('test_MATA030_CT133'))
 
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(suite)
